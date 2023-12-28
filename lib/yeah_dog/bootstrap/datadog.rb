58527654661aa1f7bf81966007bcd4c4505f60dd
Tyler Willingham <171991+tylerwillingham@users.noreply.github.com>
2023-12-28 11:10:31 -0600

    ✨ Add conventional configuration of active_support-cache service name

--- lib/yeah_dog/bootstrap/datadog.rb
+++ lib/yeah_dog/bootstrap/datadog.rb
@@ -6,14 +6,29 @@ module YeahDog
       class << self
         def apply!
           apply_global_configuration!
+          apply_caching_configuration!
         end
 
         private
 
         def apply_global_configuration!
           ::Datadog.configure do |c|
-            c.service = YeahDog.configuration.service_name
+            c.service = configuration.service_name
           end
+        end
+
+        def apply_caching_configuration!
+          ::Datadog.configure do |c|
+            c.tracing.instrument :active_support, cache_service: caching_service_name
+          end
+        end
+
+        def configuration
+          YeahDog.configuration
+        end
+
+        def caching_service_name
+          @_caching_service_name ||= "#{configuration.service_name}-cache"
         end
       end
     end
--- spec/yeah_dog/bootstrap/datadog_spec.rb
+++ spec/yeah_dog/bootstrap/datadog_spec.rb
@@ -3,7 +3,17 @@
 require 'spec_helper'
 
 RSpec.describe YeahDog::Bootstrap::Datadog do
-  let(:datadog_configuration) { spy }
+  let(:instrument_method) { spy }
+  let(:tracing_object) do
+    spy.tap do |tracing_spy|
+      allow(tracing_spy).to receive(:instrument).and_return(instrument_method)
+    end
+  end
+  let(:datadog_configuration) do
+    spy.tap do |configuration_spy|
+      allow(configuration_spy).to receive(:tracing).and_return(tracing_object)
+    end
+  end
 
   before do
     allow(Datadog).to receive(:configure).and_yield(datadog_configuration)
@@ -19,6 +29,19 @@ RSpec.describe YeahDog::Bootstrap::Datadog do
       described_class.apply!
 
       expect(datadog_configuration).to have_received(:service=).with('yeah-dog-rails-app').once
+    end
+
+    it 'sets the cache service name to [service-name]-cache' do
+      configuration = YeahDog::Configuration.new.tap do |config|
+        config.service_name = 'yeah-dog-rails-app'
+      end
+      allow(YeahDog).to receive(:configuration).and_return(configuration)
+
+      described_class.apply!
+
+      expect(tracing_object)
+        .to have_received(:instrument)
+        .with(:active_support, cache_service: 'yeah-dog-rails-app-cache')
     end
   end
 end
